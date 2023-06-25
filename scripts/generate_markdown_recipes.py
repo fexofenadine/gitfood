d9264ce9188dcf43c20637617b50a2f3b1cb9909
Hugh <fexofenadine@users.noreply.github.com>
2023-06-25 12:02:23 +1000

    Update generate_markdown_recipes.py

--- scripts/generate_markdown_recipes.py
+++ scripts/generate_markdown_recipes.py
@@ -25,7 +25,7 @@ for recipe_stub in list(all_recipe_stubs):
         if image_file == "main.jpg":
             image_width="50%"
         else:
-            image_width="33%"
+            image_width="25%"
     #    recipe_body = recipe_body.replace("{"+image_file+"}", "<img src=\"https://github.com/fexofenadine/gitfood/raw/main/recipes/"+image_path+"\" width=\"50%\" align=\"right\" />")
         recipe_body = recipe_body.replace("{"+image_file+"}", "<img src=\""+image_path+"\" width=\""+image_width+"\" align=\"right\" />")
     tag_file="./"+recipe_stub.split("/")[-3]+"/"+recipe_stub.split("/")[-2]+"/tags.txt"
