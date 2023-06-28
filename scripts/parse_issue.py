10354ff7df1d26b6537b8548a182ad115b2492d6
fexofenadine <fexofenadine@users.noreply.github.com>
2023-06-29 08:33:00 +1000

    save recipe submission to file

--- scripts/parse_issue.py
+++ scripts/parse_issue.py
@@ -1,10 +1,21 @@
 import sys
+from pathlib import Path
 
 script_name = sys.argv[0]
 issue_title = sys.argv[1]
 issue_body = sys.argv[2]
 
 print("Parsing issue with python script")
-print("Script name: "+script_name)
 print("Issue title: "+issue_title)
-print("Issue body: "+issue_body)
+recipe_title=issue_body.split("### ")[1].split("\n")[2]
+friendly_title=recipe_title.strip().replace(" ","").lower()
+print("Recipe title: "+recipe_title)
+print("Friendly title: "+friendly_title)
+
+output_file = Path("submissions/"+friendly_title+"/images/.placeholder")
+output_file.parent.mkdir(exist_ok=True, parents=True)
+output_file.write_text("placeholder text")
+
+output_file = Path("submissions/"+friendly_title+"/"+friendly_title+".raw")
+output_file.parent.mkdir(exist_ok=True, parents=True)
+output_file.write_text(issue_body)
