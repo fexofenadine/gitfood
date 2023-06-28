92b835dd081054d8615b7999dbd6729293aa0d7a
fexofenadine <fexofenadine@users.noreply.github.com>
2023-06-29 07:51:04 +1000

    upload python issue parsing script

---
+++ scripts/parse_issue.py
@@ -0,0 +1,10 @@
+import sys
+
+script_name = sys.argv[0]
+issue_title = sys.argv[1]
+issue_body = sys.argv[2]
+
+print("Parse issue with python script")
+print("Script name: "+script_name)
+print("Issue title: "+issue_title)
+print("Issue body: "+issue_body)
