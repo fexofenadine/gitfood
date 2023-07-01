d748942eb62254397f788115b083dde331f4cda9
fexofenadine <fexofenadine@users.noreply.github.com>
2023-07-01 18:32:44 +1000

    produce .recipe file from parsed markdown

--- scripts/parse_submission.py
+++ scripts/parse_submission.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import requests
 
 def get_image(image):
-    print("downloading "+image["name"])
+    print("downloading "+image["location"])
     response = requests.get(image["url"], stream=True)
     if response.status_code == 200:
         with open(image["location"], 'wb') as out_file:
@@ -20,11 +20,10 @@ else:
     with open(sys.argv[1]) as f:
         issue_body = f.read()
 
-print("Parsing issue with python script")
 recipe_title=issue_body.split("### ")[1].split("\n")[2]
 friendly_title=recipe_title.strip().replace(" ","").lower()
-print("Recipe title: "+recipe_title)
-print("Friendly title: "+friendly_title)
+
+# print("Friendly title: "+friendly_title)
 
 output_file = Path("submissions/"+friendly_title+"/images/.placeholder")
 output_file.parent.mkdir(exist_ok=True, parents=True)
@@ -34,10 +33,6 @@ recipe_file_name="submissions/"+friendly_title+"/"+friendly_title+".raw"
 output_file = Path(recipe_file_name)
 output_file.parent.mkdir(exist_ok=True, parents=True)
 output_file.write_text(issue_body)
-
-
-
-
 
 image_links=re.findall(r'!\[[^\]]*\]\((.*?)\s*?\s*\)', issue_body)
 
@@ -51,12 +46,12 @@ if image_links:
             image_name=str(i)+".jpg"
         i=i+1
         image={
-            "name":image_name,
+            "tag":"{"+image_name+"}",
             "location":"submissions/"+friendly_title+"/images/"+image_name,
             "url":image_link
         }
         images.append(image)
-        #get_image(image)
+        get_image(image)
 
 with open(recipe_file_name,'r') as f:
     body_lines = f.readlines()
@@ -66,8 +61,35 @@ for line in body_lines:
     if line[:6]=="- [X] ":
         tag=line[6:].strip().lower()
         tags.append(tag)
-        print(tag)
 
 additional_tags=issue_body.split("### Additional tags")[1].split("\n")[2].lower().replace(", ",",").replace(" ","_").split(",")
+tags=tags+additional_tags
+print("saving submissions/"+friendly_title+"/tags.txt")
 with open("submissions/"+friendly_title+"/tags.txt", 'w') as f:
-    f.write('\n'.join(map(str, tags+additional_tags)))
+    f.write('\n'.join(map(str, tags)))
+
+# heading syntax
+### Title of Recipe
+### Ingredients
+### Method
+### Tips
+
+out_title="# "+recipe_title+"\n"
+
+ingredients_text=issue_body.split("### Ingredients")[1].split("### ")[0].strip()
+ingredients_text="- ".join(("\n"+ingredients_text.lstrip()).splitlines(True))
+out_ingredients="## Ingredients:\n"+ingredients_text+"\n"
+
+method_text=issue_body.split("### Method")[1].split("### ")[0].strip()
+method_text="> ".join(("\n"+method_text.lstrip()).splitlines(True))
+out_method="## Method:\n"+method_text+"\n"
+
+tips_text=issue_body.split("### Tips")[1].split("### ")[0].strip()
+tips_text="> ".join(("\n"+tips_text.lstrip()).splitlines(True))
+out_tips="## Tips:\n"+tips_text+"\n"
+
+output=[out_title, out_ingredients, out_method, out_tips]
+print("saving submissions/"+friendly_title+"/"+friendly_title+".recipe")
+with open("submissions/"+friendly_title+"/"+friendly_title+".recipe", 'w') as f:
+    f.write("\n".join(output))
+#print("\n".join(output))
